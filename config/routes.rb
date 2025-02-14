e9c39fffada794b326620624f3700e4963cba422
ayaribon <kubotaren5213@docomo.ne.jp>
2025-02-14 17:52:12 +0900

    fix:lint

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  root 'static_pages#top'
+  root "static_pages#top"
   resources :users, only: %i[new create]
-  get 'login', to: 'user_sessions#new'
-  post 'login', to: 'user_sessions#create'
-  delete 'logout', to: 'user_sessions#destroy'
+  get "login", to: "user_sessions#new"
+  post "login", to: "user_sessions#create"
+  delete "logout", to: "user_sessions#destroy"
 end
